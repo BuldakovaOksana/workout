18f7092e0dede8b6b553b8bdebe8ea135c8a83b7
Oksana <79263143446@mail.ru>
2021-10-31 22:49:24 +0300

    taskDog

---
+++ TaskDog/Program.cs
@@ -0,0 +1,29 @@
+﻿// задача про собаку
+
+int count =0;
+int distance =10000;
+int firstFriendSpeed =1;
+int secondFriendSpeed =2;
+int dogSpeed =5;
+int friend =2;
+int Time =0;
+
+while (distance>=10)
+{
+     if (friend==1)
+     {
+         Time=distance/(firstFriendSpeed+dogSpeed);
+         friend=2;
+     }
+     else
+     {
+         Time=distance/(secondFriendSpeed+dogSpeed);
+         friend=1;
+     }
+     distance = distance -(firstFriendSpeed+secondFriendSpeed)*Time;
+     Console.WriteLine($"дистанция " + distance);
+     count++;
+     
+}
+     Console.WriteLine($"Количество кругов " + count);
+
