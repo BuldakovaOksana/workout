01bfc6ad1825ff82f299ec5150089f16832a3daa
Oksana <79263143446@mail.ru>
2021-11-01 21:59:31 +0300

    Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,33 @@
+﻿// Cформировать случайным образом целочисленный массив A из натуральных двузначных чисел. 
+// Создать на его основе масив B, отбрасывая те, которые
+// чётные
+
+int index = 0;
+int [] array= new int [10];
+
+while (index<array.Length)
+{
+    array[index]=new Random().Next(10,100);
+    index++;
+}
+index=0;
+Console.WriteLine("Массив А");
+while (index<array.Length)
+{
+     int arrayA=array[index];
+     Console.WriteLine(arrayA);
+     index++;
+}
+
+Console.WriteLine();
+Console.WriteLine("Массив B");
+index=0;
+
+while (index<array.Length)
+{
+     if (array[index]%2!=0)
+     {
+         Console.WriteLine(array[index]);
+     }
+     index++;
+}
