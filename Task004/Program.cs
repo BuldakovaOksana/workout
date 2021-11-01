ec719e61169b1ff4c97627e532649ba51b6033d7
Oksana <79263143446@mail.ru>
2021-11-01 23:27:34 +0300

    Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,32 @@
+﻿// Имеется числовой массив A заполненный числами из отрезка [minValue; maxValue]. 
+// Создать на его основе масив B, отбрасывая те, которые нарушают порядок
+// возрастания
+
+int index = 0;
+int[]array=new int [10];
+int minValue=-100;
+int maxValue=100;
+Console.WriteLine("Массив А");
+
+while (index<array.Length)
+{
+     array[index]=new Random().Next(minValue, maxValue+1);
+     Console.WriteLine(array[index]);
+     index++;
+}
+
+Console.WriteLine();
+Console.WriteLine("Массив B");
+index=0;
+int count=array[0];
+Console.WriteLine(count);
+while (index<array.Length)
+{
+     if (array[index]>count)
+     {
+         Console.WriteLine(array[index]);
+         count=array[index];
+         
+     }
+     index++;
+}
